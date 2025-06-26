6f7be692da916c109ce447471390cca3f51f41f1
Gudulsson <ludvigschonbeck@gmail.com>
2025-06-26 10:34:40 +0200

    Update sendEmail.js

--- email/sendEmail.js
+++ email/sendEmail.js
@@ -1,23 +1,22 @@
 const nodemailer = require('nodemailer');
 
-async function sendEmail(subject, htmlBody) {
+async function sendEmail(subject, htmlContent) {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+      pass: process.env.EMAIL_PASS
+    }
   });
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_TO,
     subject,
-    html: htmlBody,
+    html: htmlContent
   };
 
   await transporter.sendMail(mailOptions);
-  console.log('✅ E-post skickat!');
 }
 
 module.exports = sendEmail;
